454cdac4b388cfc986746e2235d1b338dfb95f12
SIWAKORN NOISUNDOD <63010918@KMITL.AC.TH>
2020-09-13 11:23:11 +0700

    NewOne

--- WEEK5.3/Source.cpp
+++ WEEK5.3/Source.cpp
@@ -1,19 +1,21 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+int main() {
+    int num, r, sum = 0, temp;
+    cout<<"Input  a number: ";
+    cin >> num;
 
-int main()
-{
-	int num, i;
-	long int fact = 1;
+    for (temp = num; num != 0; num = num / 10) {
+        r = num % 10;
+        sum = sum + (r * r * r);
+    }
+    if (num < 0)
+    {
+        cout << "Error";
+    }
+    else if (sum == temp)
+        cout << temp << " is an Armstrong number.\n";
+    else
+        cout << temp << " isn't an Armstrong number.\n";
 
-	cout << "Enter an integer number: ";
-	cin >> num;
-
-	for (i = num; i >= 1; i--)
-		fact = fact * i;
-
-	cout << "Factorial of " << num << " is = " << fact << endl;
-
-	return 0;
-}
 }
